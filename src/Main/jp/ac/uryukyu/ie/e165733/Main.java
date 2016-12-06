217dcdb0bcc4ae684c4ed6832585250fbb6210ff
Tatsuki Ishigaki <e165733@ie.u-ryukyu.ac.jp>
2016-12-06 16:19:38 +0900

    step0

--- src/Main/jp/ac/uryukyu/ie/e165733/Main.java
+++ src/Main/jp/ac/uryukyu/ie/e165733/Main.java
@@ -1,12 +1,8 @@
 package jp.ac.uryukyu.ie.e165733;
 
 public class Main {
-    public static void main(String[] args) { 
-        String str = null; 
-        try {
-            System.out.println(str.length());
-        } catch (NullPointerException e){
-            e.printStackTrace();
-        }
+    public static void main(String[] args) {
+        String str = "3．14";
+        Double value = Double.parseDouble(str);
     }
-}    
+}
